e846131cda58bc1fa31473894863565548c95bf4
Shmoue <samy.bury@gmail.com>
2025-01-24 16:24:10 +0100

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+            <h1 className="text - 2xl text-white underline">
+                Hello threejs
+            </h1>
+        </main>
+    );
+};
+
+export default App;
